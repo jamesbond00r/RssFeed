28aa76eb435a7f771f0fd8fcae37ac402ae160ef
Robert Clark <robert.clark@greatsplainsmfg.com>
2022-09-07 16:08:15 -0500

    use view holder pattern

--- app/src/main/java/com/example/rssfeed/FeedAdapter.kt
+++ app/src/main/java/com/example/rssfeed/FeedAdapter.kt
@@ -8,6 +8,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+class ViewHolder(v: View){
+    val tvName: TextView = v.findViewById(R.id.tvName)
+    val tvArtist: TextView = v.findViewById(R.id.tvArtist)
+    val tvSummary: TextView = v.findViewById(R.id.tvSummary)
+}
+
+
 class FeedAdapter(context: Context, private val resource: Int, private val applications: List<FeedEntry>): ArrayAdapter<FeedEntry>(context, resource) {
     private val Tag = "FeedAdapter"
     private val inflater = LayoutInflater.from(context)
@@ -20,20 +27,23 @@ class FeedAdapter(context: Context, private val resource: Int, private val appli
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         Log.d(Tag, "getView() called")
         val view: View
+        val viewHolder: ViewHolder
         if (convertView == null){
             view = inflater.inflate(resource, parent, false)
-        } else view = convertView
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else{
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
 
 
-        val tvName: TextView = view.findViewById(R.id.tvName)
-        val tvArtist: TextView = view.findViewById(R.id.tvArtist)
-        val tvSummary: TextView = view.findViewById(R.id.tvSummary)
 
         val currentApp = applications[position]
 
-        tvName.text = currentApp.name
-        tvArtist.text = currentApp.artist
-        tvSummary.text = currentApp.summary
+        viewHolder.tvName.text = currentApp.name
+        viewHolder.tvArtist.text = currentApp.artist
+        viewHolder.tvSummary.text = currentApp.summary
 
         return view
 
